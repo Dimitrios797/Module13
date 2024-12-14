1f7a31de0f712a6516d22267e208cd012feafd1d
Dimitrios797 <146129395+Dimitrios797@users.noreply.github.com>
2024-12-14 20:46:15 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,64 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.utils import executor
+
+
+API_TOKEN = '7897509856:AAGzyktr7yNuVXHVsofX0Hj_mEMVJn_RvRE'
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Привет! Напишите 'Calories', чтобы начать.")
+
+
+@dp.message_handler(lambda message: message.text == 'Calories')
+async def set_age(message: types.Message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+
+    data = await state.get_data()
+    age = int(data.get('age'))
+    growth = int(data.get('growth'))
+    weight = int(data.get('weight'))
+
+
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5
+
+    await message.answer(f'Ваша норма калорий: {calories:.2f} ккал.')
+
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
