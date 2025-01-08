d92bb0888ec03d1e0a0dd4cb4ca0af994e6e8adf
Dimitrios797 <146129395+Dimitrios797@users.noreply.github.com>
2025-01-08 16:39:50 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -55,3 +55,46 @@ def add_sample_products():
 
 # Вызовите эту функцию один раз, чтобы добавить продукты
 add_sample_products()
+def get_all_products():
+    # Подключение к базе данных
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    # Получение всех записей из таблицы Products
+    cursor.execute('SELECT * FROM Products')
+    products = cursor.fetchall()
+
+    # Закрытие соединения
+    conn.close()
+
+    return products
+
+def add_user(username, email, age):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    # Проверка существования пользователя по имени
+    cursor.execute('SELECT * FROM Users WHERE username = ?', (username,))
+    existing_user = cursor.fetchone()
+
+    if existing_user is None:
+        # Вставка нового пользователя
+        cursor.execute('''
+        INSERT INTO Users (username, email, age, balance) 
+        VALUES (?, ?, ?, ?)''', (username, email, age, 1000))
+        conn.commit()
+    else:
+        print(f"Пользователь с именем '{username}' уже существует.")
+
+    conn.close()
+
+def is_included(username):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM Users WHERE username = ?', (username,))
+    user = cursor.fetchone()
+
+    conn.close()
+
+    return user is not None
